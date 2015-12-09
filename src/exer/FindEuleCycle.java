e7a172c569f749f9ced40c8f9f204e733dadd469
amysue <staramy_2005@126.com>
2015-12-09 15:31:24 +0800

    Eulerian tour finish

--- src/exer/FindEuleCycle.java
+++ src/exer/FindEuleCycle.java
@@ -1,48 +1,59 @@
 package exer;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import edu.princeton.cs.algs4.*;
+
 public class FindEuleCycle {
     public boolean euleCycle;
-    private ST<Integer, Boolean>[] marked;
+    private List<ST<Integer, Boolean>> marked;
     private Stack<Integer> vers;
     private boolean[] connected;
     private int count;
     private int edges;
     private int startV;
-    public FindEuleCycle(Graph G) {
-        euleCycle = true;
+    public FindEuleCycle(Graph G, int start) {
+        euleCycle = false;
         if (!evenDegree(G)) {
-            euleCycle = false;
+            System.out.println("The Graph degress is not even");
             return;
         }
         connected = new boolean[G.V()];
         if (!connected(G)) {
-            euleCycle = false;
+            System.out.println("the Graph is not Connected");
             return;
         }
-        marked = (ST<Integer, Boolean>[]) new ST[G.V()];
+        marked = new ArrayList<ST<Integer, Boolean>>();
         for (int v = 0; v < G.V(); v++) {
-            marked[v] = new ST<Integer, Boolean>();
+            marked.add(v, new ST<Integer, Boolean>());
         }
         vers = new Stack<>();
         edges = 0;
-        startV = 0;
+        startV = start;
         findCycle(G, startV, -1);
     }
     private void findCycle(Graph G, int v, int u) {
         vers.push(v);
+        ST<Integer, Boolean> item = marked.get(v);
         for (int w: G.adj(v)) {
-            if (!marked[v].contains(w)) {
-                marked[v].put(w, true);
-                marked[w].put(v, true);
+            if (!item.contains(w) || !item.get(w)) {
+                item.put(w, true);
+                marked.get(w).put(v, true);
                 edges++;
                 findCycle(G, w, v);
             }
         }
-        if (edges == G.E() && v == startV) {
+        if (euleCycle || (edges == G.E() && v == startV)) {
+            euleCycle = true;
             return;
         }
         else {
             vers.pop();
+            marked.get(u).put(v, false);  
+            item.put(u, false);
+            edges--;
+            return;
         }
     }
     private void dfs (Graph G, int v) {
@@ -75,12 +86,20 @@ public class FindEuleCycle {
     public boolean isEuleCycle() {
         return euleCycle;
     }
+    public Iterator<Integer> path() {
+        return vers.iterator();
+    }
     public static void main(String[] args) {
         In in = new In(args[0]);
+//        In in = new In("D:\\GIT\\Algs\\data\\eulecycle1.txt");
         Graph G  = new Graph(in);
-        FindEuleCycle fec = new FindEuleCycle(G);
+        FindEuleCycle fec = new FindEuleCycle(G, Integer.parseInt(args[1]));
         if (fec.isEuleCycle()) {
-            
+            Iterator<Integer> path = fec.path();
+            while(path.hasNext()) {
+                System.out.print(path.next() + " ");
+            }
+            System.out.println();
         }
         else {
             System.out.println("The Graph is not Eule Cycle");
